7555974e795ff69dfe6370a80bac0646b757d591
M. Rizal Marsaoly <m.rizalmarsaoly@gmail.com>
2021-07-16 11:43:14 +0900

    add styles html file and new feature api

--- huawei-api.py
+++ huawei-api.py
@@ -3,28 +3,58 @@ import huaweisms.api.user as user
 import huaweisms.api.wlan as wlan
 import huaweisms.api.sms as sms
 import huaweisms.api.device as device
+import huaweisms.api.dialup as dialup
+import huaweisms.api.monitoring as mon
+ 
+import sys, os
+import json
+
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
-
-ctx = user.quick_login("admin", "redkocin", "192.168.8.1")
+def ctx():
+    con =  user.quick_login("admin", "redkocin", "192.168.8.1")
+    return con
 
 def getsms():
-    return sms.get_sms(ctx,1)
+    smss = sms.get_sms(ctx(),1)
+    return smss
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
 
+@app.route("/mon")
+def mon_():
+    return mon.status(ctx())
+
 @app.route("/getsms")
 def hello():
     return getsms()
 
+@app.route("/modemoff")
+def modemoff():
+    return dialup.switch_mobile_off(ctx())
+
+@app.route("/modemon")
+def modemon():
+    return dialup.switch_mobile_on(ctx())
+
+@app.route("/modemstatus")
+def modemstatus():
+    return  json.dumps({"status": dialup.get_mobile_status(ctx())})
+
 @app.route("/reboot")
 def reboot():
-    device.reboot(ctx)
-    return "<p>Device rebooted!</p>"
+    return json.dumps({"reboot" : device.reboot(ctx())})
+
+@app.route("/relogin")
+def relogin():
+    return   json.dumps({"msg" :user.login(ctx(), 'admin','redkocin')})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
